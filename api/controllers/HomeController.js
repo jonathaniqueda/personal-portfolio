c86274244a724b8b978d6a137d75957a69b62c08
Jonathan Iqueda <jonathaniqueda@me.com>
2016-12-27 17:55:43 -0200

    first commit

---
+++ api/controllers/HomeController.js
@@ -0,0 +1,12 @@
+/**
+ * HomeController
+ *
+ * @description :: Server-side logic for managing Homes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+    index: function(req, res) {
+        return res.view('homepage/index');
+    },
+};
