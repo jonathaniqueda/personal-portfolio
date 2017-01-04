5a4fc0b5a257c4e697b4db346fec6c112ce83705
Jonathan Iqueda <jonathaniqueda@me.com>
2017-01-04 11:54:50 -0200

    envs

--- config/connections.js
+++ config/connections.js
@@ -34,22 +34,6 @@ module.exports.connections = {
 
     /***************************************************************************
      *                                                                          *
-     * MySQL is the world's most popular relational database.                   *
-     * http://en.wikipedia.org/wiki/MySQL                                       *
-     *                                                                          *
-     * Run: npm install sails-mysql                                             *
-     *                                                                          *
-     ***************************************************************************/
-    // someMysqlServer: {
-    //   adapter: 'sails-mysql',
-    //   host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    //   user: 'YOUR_MYSQL_USER', //optional
-    //   password: 'YOUR_MYSQL_PASSWORD', //optional
-    //   database: 'YOUR_MYSQL_DB' //optional
-    // },
-
-    /***************************************************************************
-     *                                                                          *
      * MongoDB is the leading NoSQL database.                                   *
      * http://en.wikipedia.org/wiki/MongoDB                                     *
      *                                                                          *
@@ -65,28 +49,13 @@ module.exports.connections = {
         database: 'portfolio_iqueda' //optional
     },
 
-    /***************************************************************************
-     *                                                                          *
-     * PostgreSQL is another officially supported relational database.          *
-     * http://en.wikipedia.org/wiki/PostgreSQL                                  *
-     *                                                                          *
-     * Run: npm install sails-postgresql                                        *
-     *                                                                          *
-     *                                                                          *
-     ***************************************************************************/
-    // somePostgresqlServer: {
-    //   adapter: 'sails-postgresql',
-    //   host: 'YOUR_POSTGRES_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    //   user: 'YOUR_POSTGRES_USER', // optional
-    //   password: 'YOUR_POSTGRES_PASSWORD', // optional
-    //   database: 'YOUR_POSTGRES_DB' //optional
-    // }
-
-
-    /***************************************************************************
-     *                                                                          *
-     * More adapters: https://github.com/balderdashy/sails                      *
-     *                                                                          *
-     ***************************************************************************/
+    prodMongoServer: {
+        adapter: 'sails-mongo',
+        host: '45.55.79.241',
+        port: 27017,
+        user: '', //optional
+        password: '', //optional
+        database: 'portfolio_iqueda' //optional
+    },
 
 };
