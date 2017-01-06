eff13c9ee48671ee9c82f58335fe692c858715b5
Jonathan Iqueda <jonathaniqueda@me.com>
2017-01-06 16:15:13 -0200

    timeout

--- config/local.js
+++ config/local.js
@@ -1,4 +1,5 @@
 module.exports = {
     port: process.env.PORT || 1337,
-    environment: process.env.NODE_ENV || 'development'
+    environment: process.env.NODE_ENV || 'development',
+    hookTimeout: 30000 // 30 seconds
 };
